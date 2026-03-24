cb30d494d3935b06a17702efa93f5f1198fb9965
MingkangW <tsingmicro@gmail.com>
2026-03-24 15:28:26 +0000

    移除keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,8 +3,3 @@
 -- Add any additional keymaps here
 
 -- Terminal mode window navigation (for Claude sidebar and other terminals)
-vim.keymap.set("t", "<C-w>h", "<C-\\><C-n><C-w>h", { desc = "Go to left window" })
-vim.keymap.set("t", "<C-w>j", "<C-\\><C-n><C-w>j", { desc = "Go to lower window" })
-vim.keymap.set("t", "<C-w>k", "<C-\\><C-n><C-w>k", { desc = "Go to upper window" })
-vim.keymap.set("t", "<C-w>l", "<C-\\><C-n><C-w>l", { desc = "Go to right window" })
-vim.keymap.set("t", "<C-w><C-w>", "<C-\\><C-n><C-w><C-w>", { desc = "Go to next window" })
